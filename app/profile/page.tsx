7b0d3c207bbd2137688c4e15aee935bc5707e39c
@Mr_DeshzCoder <94686812+GayashanDeshapriya@users.noreply.github.com>
2024-03-30 23:11:38 +0530

    Revert "changes"

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import Layout from '../components/layout'
-
-
 export default function Profile(){
     return(
-        <Layout>
         <div>
             <h1>Profile</h1>
         </div>
-    </Layout>
     )
 }
