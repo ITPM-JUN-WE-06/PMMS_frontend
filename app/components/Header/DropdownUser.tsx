2883368bbfbe813027610760bdb37bc7a8dafda6
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2024-03-31 09:29:54 +0530

    last update

--- app/components/Header/DropdownUser.tsx
+++ app/components/Header/DropdownUser.tsx
@@ -44,9 +44,9 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            Gayashan Deshapriya
           </span>
-          <span className="block text-xs">UX Designer</span>
+          <span className="block text-xs">Cordinator</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
@@ -84,7 +84,7 @@ const DropdownUser = () => {
         ref={dropdown}
         onFocus={() => setDropdownOpen(true)}
         onBlur={() => setDropdownOpen(false)}
-        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${
+        className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-lg text-text-light border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark  ${
           dropdownOpen === true ? "block" : "hidden"
         }`}
       >
